db50a42d5359e7597dd189cb6a275c5003b476a2
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-04-14 04:56:52 +0800

    feat(自定义参数序列化):

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from '../../src/index'
 import 'nprogress/nprogress.css'
 import NProgress from 'nprogress'
+import qs from 'qs'
 
 // document.cookie = 'a=b'
 
@@ -29,102 +30,134 @@ import NProgress from 'nprogress'
 //     console.log(res)
 // })
 
-const instance = axios.create()
+// const instance = axios.create()
 
-function calculatePercentage(loaded: number, total: number) {
-    return Math.floor(loaded * 1.0) / total
-}
+// function calculatePercentage(loaded: number, total: number) {
+//     return Math.floor(loaded * 1.0) / total
+// }
 
-function loadProgressBar() {
-    const setupStartProgress = () => {
-        instance.interceptors.request.use(config => {
-            NProgress.start()
-            return config
-        })
-    }
+// function loadProgressBar() {
+//     const setupStartProgress = () => {
+//         instance.interceptors.request.use(config => {
+//             NProgress.start()
+//             return config
+//         })
+//     }
 
-    const setupUpdateProgress = () => {
-        const update = (e: ProgressEvent) => {
-            console.log(e)
-            NProgress.set(calculatePercentage(e.loaded, e.total))
-        }
-        instance.defaults.onDownloadProgress = update
-        instance.defaults.onUploadProgress = update
-    }
+//     const setupUpdateProgress = () => {
+//         const update = (e: ProgressEvent) => {
+//             console.log(e)
+//             NProgress.set(calculatePercentage(e.loaded, e.total))
+//         }
+//         instance.defaults.onDownloadProgress = update
+//         instance.defaults.onUploadProgress = update
+//     }
 
-    const setupStopProgress = () => {
-        instance.interceptors.response.use(
-            response => {
-                NProgress.done()
-                return response
-            },
-            error => {
-                NProgress.done()
-                return Promise.reject(error)
-            }
-        )
-    }
+//     const setupStopProgress = () => {
+//         instance.interceptors.response.use(
+//             response => {
+//                 NProgress.done()
+//                 return response
+//             },
+//             error => {
+//                 NProgress.done()
+//                 return Promise.reject(error)
+//             }
+//         )
+//     }
 
-    setupStartProgress()
-    setupUpdateProgress()
-    setupStopProgress()
-}
+//     setupStartProgress()
+//     setupUpdateProgress()
+//     setupStopProgress()
+// }
 
-loadProgressBar()
+// loadProgressBar()
 
-const downloadEl = document.getElementById('download')
+// const downloadEl = document.getElementById('download')
 
-downloadEl!.addEventListener('click', e => {
-    instance.get('https://img.mukewang.com/5cc01a7b0001a33718720632.jpg')
+// downloadEl!.addEventListener('click', e => {
+//     instance.get('https://img.mukewang.com/5cc01a7b0001a33718720632.jpg')
+// })
+
+// const uploadEl = document.getElementById('upload')
+
+// uploadEl!.addEventListener('click', e => {
+//     const data = new FormData()
+//     const fileEl = document.getElementById('file') as HTMLInputElement
+//     if (fileEl.files) {
+//         data.append('file', fileEl.files[0])
+
+//         instance.post('/more/upload', data)
+//     }
+// })
+
+// axios
+//     .post(
+//         '/more/post',
+//         {
+//             a: 1
+//         },
+//         {
+//             auth: {
+//                 username: 'Yee1',
+//                 password: '123456'
+//             }
+//         }
+//     )
+//     .then(res => {
+//         console.log(res)
+//     })
+
+// axios
+//     .get('/more/304')
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .catch((e: AxiosError) => {
+//         console.log(e.message)
+//     })
+
+// axios
+//     .get('/more/304', {
+//         validateStatus(status) {
+//             return status >= 200 && status < 400
+//         }
+//     })
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .catch((e: AxiosError) => {
+//         console.log(e.message)
+//     })
+
+axios.get('/more/get', {
+    params: new URLSearchParams('a=b&c=d')
+}).then(res => {
+    console.log(res)
 })
 
-const uploadEl = document.getElementById('upload')
+axios.get('/more/get', {
+    params: {
+        a: 1,
+        b: 2,
+        c: ['a', 'b', 'c']
+    }
+}).then(res => {
+    console.log(res)
+})
 
-uploadEl!.addEventListener('click', e => {
-    const data = new FormData()
-    const fileEl = document.getElementById('file') as HTMLInputElement
-    if (fileEl.files) {
-        data.append('file', fileEl.files[0])
-
-        instance.post('/more/upload', data)
+const instance = axios.create({
+    paramsSerializer(params) {
+        return qs.stringify(params, { arrayFormat: 'brackets' })
     }
 })
 
-axios
-    .post(
-        '/more/post',
-        {
-            a: 1
-        },
-        {
-            auth: {
-                username: 'Yee1',
-                password: '123456'
-            }
-        }
-    )
-    .then(res => {
-        console.log(res)
-    })
-
-axios
-    .get('/more/304')
-    .then(res => {
-        console.log(res)
-    })
-    .catch((e: AxiosError) => {
-        console.log(e.message)
-    })
-
-axios
-    .get('/more/304', {
-        validateStatus(status) {
-            return status >= 200 && status < 400
-        }
-    })
-    .then(res => {
-        console.log(res)
-    })
-    .catch((e: AxiosError) => {
-        console.log(e.message)
-    })
+instance.get('/more/get', {
+    params: {
+        a: 1,
+        b: 2,
+        c: ['a', 'b', 'c']
+    }
+}).then(res => {
+    console.log(res)
+})
