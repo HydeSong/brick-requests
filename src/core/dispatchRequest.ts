88e2347d308a3cf776718cf7dfa332ef081c6f9b
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-04-14 05:23:50 +0800

    feat(静态方法扩展):

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -13,12 +13,12 @@ export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromis
 }
 
 function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
+  config.url = transformURL(config)
   config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
-function transformUrl(config: AxiosRequestConfig): string {
+export function transformURL(config: AxiosRequestConfig): string {
   let { url, params, paramsSerializer, baseURL } = config
   if (baseURL && !isAbsoluteURL(url!)) {
     url = combineURL(baseURL, url)
