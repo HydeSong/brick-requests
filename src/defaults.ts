882d7c11c79bdf9b069044bc024b465af3c4f1ae
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-04-14 00:55:52 +0800

    feat(请求和响应配置化): 实现transformRequest transformResponse

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,4 +1,6 @@
 import { AxiosRequestConfig } from './types'
+import { processHeaders } from './helpers/headers'
+import { transformRequest, transformResponse } from './helpers/data'
 
 const defaults: AxiosRequestConfig = {
   method: 'get',
@@ -8,8 +10,17 @@ const defaults: AxiosRequestConfig = {
       Accept: 'application/json, text/plain, */*'
     }
   },
-  transformRequest: [],
-  transformResponse: []
+  transformRequest: [
+    function(data: any, headers: any): any {
+      processHeaders(headers, data)
+      return transformRequest(data)
+    }
+  ],
+  transformResponse: [
+    function(data: any, headers: any): any {
+      return transformResponse(data)
+    }
+  ]
 }
 
 const methodsNoData = ['delete', 'get', 'head', 'options']
