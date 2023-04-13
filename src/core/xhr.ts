4901224ece9fe06151aaf1f572bdef33d0fc5989
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-04-14 07:14:32 +0800

    [build]

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -122,10 +122,17 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     function processCancel(): void {
       if (cancelToken) {
-        cancelToken.promise.then(reason => {
-          request.abort()
-          reject(reason)
-        })
+        cancelToken.promise
+          .then(reason => {
+            request.abort()
+            reject(reason)
+          })
+          .catch(
+            /* istanbul ignore next */
+            () => {
+              // do nothing
+            }
+          )
       }
     }
 
