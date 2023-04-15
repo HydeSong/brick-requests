d22c7c86a3bb12107e0d10641a65fb2314378f75
hyde <hyde.song@yahoo.com>
2023-04-15 15:56:49 +0800

    fix(npm run build error):

--- src/cancel/CancelToken.ts
+++ src/cancel/CancelToken.ts
@@ -13,6 +13,7 @@ export default class CancelToken {
     let resolvePromise: ResolvePromise
 
     this.promise = new Promise<Cancel>(resolve => {
+      // @ts-ignore
       resolvePromise = resolve
     })
 
